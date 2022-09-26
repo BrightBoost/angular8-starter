800d5f1a8f83c08bd10cdbfb9efbbf16e72eefac
brightboost <info@brightboost.nl>
2022-09-26 20:29:15 +0200

    routing from component

--- start-app/src/app/app.module.ts
+++ start-app/src/app/app.module.ts
@@ -4,17 +4,21 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { SomethingComponent } from './something/something.component';
 
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "about", component: AboutComponent }
+  { path: "about", component: AboutComponent },
+  { path: "about/something", component: SomethingComponent }
+
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    SomethingComponent
   ],
   imports: [
     BrowserModule,
